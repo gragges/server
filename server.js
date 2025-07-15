c9a54fbaf333c93b9dd67a3eff427ac4a30e2332
gragges <deskanataras@gmail.com>
2025-07-15 13:15:17 +0200

    pupet

--- server.js
+++ server.js
@@ -2,10 +2,47 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const puppeteer = require('puppeteer');
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  scrape().then((data) => {
+    console.log(data);
+    res.send(data);
+  }).catch((error) => {
+    console.error('Error during scraping:', error);
+    res.status(500).send('Error during scraping');
+  });
 })
 
 app.listen(port, () => {
   console.log('Server is running on ');
 });
+
+const scrape = async ()=>{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const page2=await browser.newPage();
+
+    const url="https://www.bankier.pl/gielda/notowania/akcje"
+    const url2="https://www.bankier.pl/gielda/notowania/new-connect"
+
+    await page.goto(url);
+    await page2.goto(url2)
+
+
+    //const title=await page.title()
+    //console.log(`To jest tytul: ${title}`)
+    const dane = await page.evaluate(()=>{
+        const zmienna = document.querySelectorAll('.colWalor.textNowrap');
+        //const zmienna = document.querySelectorAll('.colLiczbaTransakcji');
+        
+        return Array.from(zmienna).map((walor)=>{
+            const tytul=walor.innerText
+            //const transakcje=walor.getElementsByTagName('td').querySelectorAll('.colLiczbaTransakcji').innerText;
+            return tytul
+               
+            
+        })
+    
+});}
