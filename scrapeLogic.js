84ebf1cde8ee9602f37000d0190758891b5e99fb
gragges <deskanataras@gmail.com>
2025-07-16 12:40:32 +0200

    dane

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,5 +1,19 @@
+const puppeteer = require('puppeteer');
+
 const scrapeLogic =(res) => {
-    res.send('Hello from scrapeLogic!');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.bankier.pl/gielda/notowania/akcje');
+
+    await page.waitForSelector('.colWalor.textNowrap');
+    await page.setViewport({ width: 1200, height: 800 });
+    const data = await page.evaluate(() => {
+        const elements = document.querySelectorAll('.colWalor.textNowrap');
+        return Array.from(elements).map(element => element.innerText);
+    });
+    res.send(`Hello from scrapeLogic! ${data}`);
+    browser.close();
+    console.log('Scraping completed');
 }
 
 module.exports = {scrapeLogic};
